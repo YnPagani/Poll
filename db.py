1327d80f01cedcd36690998fe7e36cdc57b2b18f
Yuri Pagani <yuripagani@gmail.com>
2021-04-24 20:54:48 -0300

    Refactored: the database interface has been simplified to work better with model classes

--- db.py
+++ db.py
@@ -1,5 +1,3 @@
-from psycopg2.extras import execute_values
-
 # Queries
 CREATE_POLLS = """CREATE TABLE IF NOT EXISTS polls(
     id SERIAL PRIMARY KEY,
@@ -20,44 +18,25 @@ CREATE_VOTES = """CREATE TABLE IF NOT EXISTS votes(
     FOREIGN KEY(option_id) REFERENCES options(id)
     );"""
 
+SELECT_POLL = """SELECT * FROM polls WHERE id = %s;"""
+
+SELECT_OPTION = """SELECT * FROM options WHERE id = %s"""
+
 SELECT_ALL_POLLS = """SELECT * FROM polls;"""
 
-SELECT_POLL_WITH_OPTIONS = """SELECT * 
-    FROM polls 
-    JOIN options 
-    ON polls.id = options.poll_id 
-    WHERE polls.id = %s;"""
+SELECT_POLL_OPTIONS = """SELECT * FROM options WHERE poll_id = %s;"""
 
-SELECT_LATEST_POLL = """SELECT *
+SELECT_LATEST_POLL = """SELECT * 
     FROM polls
-    JOIN options
-    ON polls.id = options.poll_id
     WHERE polls.id = (SELECT id FROM polls ORDER BY id DESC LIMIT 1);"""
 
-SELECT_RANDOM_VOTE = """SELECT *
-    FROM votes
-    WHERE option_id = %s
-    ORDER BY RANDOM()
-    LIMIT 1;"""
-
-SELECT_POLL_VOTE_DETAILS = """SELECT
-    options.id,
-    options.option_text,
-    COUNT(votes.option_id) 
-    AS vote_count,
-    COUNT(votes.option_id) / SUM(COUNT(votes.option_id)) OVER() * 100.0 
-    AS vote_percentage
-    FROM options
-    LEFT JOIN votes
-    ON options.id = votes.option_id
-    WHERE options.poll_id = %s
-    GROUP BY options.id;"""
+SELECT_VOTES_FOR_OPTION = "SELECT * FROM votes WHERE option_id = %s"
 
 INSERT_POLL_RETURN_ID = """INSERT INTO polls(title, owner_username)
     VALUES (%s, %s) RETURNING id;"""
 
-INSERT_OPTION = """INSERT INTO options(option_text, poll_id) 
-    VALUES %s;"""
+INSERT_OPTION_RETURN_ID = """INSERT INTO options(option_text, poll_id) 
+    VALUES %s RETURNING id;"""
 
 INSERT_VOTE = """INSERT INTO votes(username, option_id)
     VALUES (%s, %s);"""
@@ -73,7 +52,9 @@ def create_tables(connection):
             cursor.execute(CREATE_VOTES)
 
 
-def create_poll(connection, title, owner, options):
+# -------- POLL ----------
+
+def create_poll(connection, title, owner):
     with connection:
         with connection.cursor() as cursor:
             # cursor will have the id of the new poll
@@ -82,14 +63,7 @@ def create_poll(connection, title, owner, options):
             # Fetch from the cursor the id of the new poll
             ID = 0
             poll_id = cursor.fetchone()[ID]
-
-            # Create list of tuple containing info for each option
-            option_values = [(option_text, poll_id) for option_text in options]
-
-            # Psycopg2 helper method to execute multiple queries at once. This
-            # method will execute INSERT_OPTION for each tuple in the option_va
-            # lue using the cursor we created.
-            execute_values(cursor, INSERT_OPTION, option_values)
+            return poll_id
 
 
 def add_poll_vote(connection, username, option_id):
@@ -105,30 +79,49 @@ def get_polls(connection):
             return cursor.fetchall()
 
 
+def get_poll(connection, poll_id: int):
+    with connection:
+        with connection.cursor as cursor:
+            cursor.execute(SELECT_POLL, (poll_id,))
+            return cursor.fetchone()
+
+
 def get_latest_poll(connection):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(SELECT_LATEST_POLL)
-            return cursor.fetchall()
-
-
-def get_polls_details(connection, poll_id):
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(SELECT_POLL_WITH_OPTIONS, (poll_id,))
-            return cursor.fetchall()
-
-
-def get_poll_and_results(connection, poll_id):
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(SELECT_POLL_VOTE_DETAILS, (poll_id,))
-            return cursor.fetchall()
-
-
-def get_random_poll_vote(connection, option_id):
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(SELECT_RANDOM_VOTE, (option_id,))
             return cursor.fetchone()
 
+
+# ---------- OPTIONS -----------
+
+def get_option(connection, option_id: int):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_OPTION, (option_id,))
+            return cursor.fetchone()
+
+
+def add_option(connection, option_text: str, poll_id: id):
+    ID = 0  # index
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_OPTION_RETURN_ID, (option_text, poll_id))
+            option_id = cursor.fetchone()[ID]
+            return option_id
+
+
+# ---------- VOTES -------------
+
+def get_votes_for_option(connection, option_id: int):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_VOTES_FOR_OPTION, (option_id,))
+            return cursor.fetchall()
+
+
+def get_poll_options(connection, poll_id):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_POLL_OPTIONS, (poll_id,))
+            return cursor.fetchall()
