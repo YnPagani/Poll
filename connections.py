e3de83022418809cc590e34b7f69527f223568e9
Yuri Pagani <yuripagani@gmail.com>
2021-04-25 16:06:50 -0300

    Refactored: Added connection pool

--- connections.py
+++ connections.py
@@ -1,9 +1,8 @@
 import os
-import psycopg2
+from psycopg2.pool import SimpleConnectionPool
 from dotenv import load_dotenv
 
 load_dotenv()
+db_uri = os.environ.get("DATABASE_URI")
 
-
-def create_connection():
-    return psycopg2.connect(os.environ.get("DATABASE_URI"))
+pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=db_uri)
